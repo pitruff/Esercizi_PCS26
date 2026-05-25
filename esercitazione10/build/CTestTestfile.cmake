f48d49c39a4a6ae47d9992b23de85714281a0230
Pietro Ruffatti Vitrotti <s322193@studenti.polito.it>
2026-05-25 21:37:35 +0000

    Consegna Esercitazione 10 (avevo accidentalmente lasciato la cartella build, ora l'ho rimossa)

--- esercitazione10/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pcsdocker/Data/Esercizi_PCS26/esercitazione10
-# Build directory: /home/pcsdocker/Data/Esercizi_PCS26/esercitazione10/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[prova_GC]=] "/home/pcsdocker/Data/Esercizi_PCS26/esercitazione10/build/gradiente_coniugato")
-set_tests_properties([=[prova_GC]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/pcsdocker/Data/Esercizi_PCS26/esercitazione10/CMakeLists.txt;11;add_test;/home/pcsdocker/Data/Esercizi_PCS26/esercitazione10/CMakeLists.txt;0;")
